18fc7ff788153aed7e42785842895070fad78076
David Mulhern <david@mulhern.me>
2016-05-18 17:20:10 -0700

    Working on Base Dart application
    Getting things sorted

---
+++ lib/index-component.dart
@@ -0,0 +1,6 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'david-app',
+    template: 'David Mulhern')
+class IndexComponent {}
--- web/david.dart
+++ web/david.dart
@@ -1,6 +1,5 @@
 import 'package:angular2/platform/browser.dart';
-import 'package:david/app-component.dart';
-
+import 'package:david/index-component.dart';
 void main() {
-  bootstrap(AppComponent);
+  bootstrap(IndexComponent);
 }
