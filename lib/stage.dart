2c6724221fe6a016a3dcc7bf62a5d669231d1927
David Mulhern <david@mulhern.me>
2016-05-22 16:53:43 -0700

    Adding sprites

--- lib/stage.dart
+++ lib/stage.dart
@@ -1,7 +1,9 @@
 import 'package:angular2/core.dart';
+import 'package:david/sprites/puppets/david.dart';
 
 @Component(
     selector: 'david-app',
+    directives: [DavidSpriteComponent],
     templateUrl: 'package:david/stage.html',
     styleUrls: const ['package:david/stage.css'])
 class StageComponent {}
